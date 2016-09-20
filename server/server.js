5ea9f6b562455bb4b622d50dffd2882b44432bd6
Craig Moreno <Craig Moreno>
2016-09-20 02:24:13 -0700

    Added initial tests

--- server/server.js
+++ server/server.js
@@ -2,17 +2,14 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
-
 var root = path.normalize(__dirname + '/..');
 
 app.set('clientPath', path.join(root, 'client'));
-console.log(app.get('clientPath'));
 app.use(express.static(app.get('clientPath')));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.resolve(app.get('clientPath') + '/index.html'));
+    throw 'Error';
+    // res.sendFile(path.resolve(app.get('clientPath') + '/index.html'));
 });
 
-app.listen('3000', function() {
-    console.log('Listening on port 3000');
-});
+module.exports = app;
