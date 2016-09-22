cbab287aa3906774a8cfff1c93220b4aac42b030
Craig Moreno <craig@bodyhigh.com>
2016-09-22 03:24:29 -0700

    Updated Redis Client config for RedisToGo

--- server/database/redisConnection.js
+++ server/database/redisConnection.js
@@ -1,13 +1,14 @@
-var redisConfig = {
-    host: '127.0.0.1',
-    post: '6379',
-    db: '1'
-};
-
 var env = process.env.NODE_ENV || 'development';
 
 var redis = require('redis');
-var client = redis.createClient();
+
+if (process.env.REDISTOGO_URL) {
+    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+    var client = redis.createClient(rtg.port, rtg.hostname);
+    redis.auth(rtg.auth.split(":")[1]);
+} else {
+    var client = redis.createClient();
+}
 
 if (env === 'test') {
     client.select(env.length);
--- server/server.js
+++ server/server.js
@@ -4,15 +4,15 @@ var path = require('path');
 var app = express();
 var root = path.normalize(__dirname + '/..');
 
-// app.get('/', function(req, res) {
-//     // throw 'Some error goes here';
-//     res.status(200).send('Hello world');
-// });
-
 app.set('clientPath', path.join(root, 'client'));
 app.use(express.static(app.get('clientPath')));
 
 app.use('/api', require('./api/routes'));
+
+app.use('/environment', function(req, res) {
+    var env = process.env.NODE_ENV || 'development';
+    res.status(200).json(env);
+});
 
 app.get('/*', function(req, res) {
     res.sendFile(path.resolve(app.get('clientPath') + '/index.html'));
